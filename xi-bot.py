2b7a70efff4c4339b53fc9e91800f7706c8541c8
Xi Han <xihan94@ucla.edu>
2020-02-28 18:57:39 -0800

    Come and go

--- xi-bot.py
+++ xi-bot.py
@@ -6,6 +6,7 @@ from util.time import get_formatted_timestamp_in_LA as timestamp
 from alias.cmd_set_alias import exec_set_alias, get_alias
 
 client = discord.Client()
+voice_client = None
 
 @client.event
 async def on_ready():
@@ -18,6 +19,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global voice_client
     
     if message.content.startswith('!hello'):
         await message.channel.send('Hello, {}!'.format(get_alias(message.author.name, message.author.discriminator)))
@@ -33,6 +35,22 @@ async def on_message(message):
             await message.channel.send('Not enough privilege.')
         else:
             await exec_set_alias(client, message.channel, argv[1], argv[2], argv[3])
+
+    if message.content.startswith('!come'):
+        channel = message.author.voice.channel
+
+        if channel:
+            if voice_client:
+                await voice_client.disconnect()
+            voice_client = None
+            voice_client = await channel.connect()
+
+
+    if message.content.startswith('!go'):
+        if voice_client:
+            await voice_client.disconnect()
+            voice_client = None
+
 
 @client.event
 async def on_member_join(member):
